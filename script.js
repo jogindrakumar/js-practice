f3c1a621d77aa66741866225b44f2f1c8bbeb108
jogindrakumar <joginder9868@outlook.com>
2024-01-16 06:50:55 +0530

    DOM-practice

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-var btn = document.querySelectorAll('button');
-btn.addEventListener('click',function(e) {
+// var btn = document.querySelector("button");
+// var p = document.querySelector("p");
+// btn.addEventListener('click',function() {
 
-    
-} );
+//      p.textContent += "hey joginder";
+//     p.style.color = "red";
+//     p.innerHTML = "<b>hello</b>"
+// } );
+
+
+var img1 = document.querySelector("#img1");
+
+var img2 = document.querySelector("#img2");
+var btn = document.querySelector("button");
+
+
+btn.addEventListener("click", function(){
+// img1.src = "https://cdn.pixabay.com/photo/2024/01/04/21/55/mountain-8488489_1280.jpg";
+// img2.src = "https://cdn.pixabay.com/photo/2023/10/21/06/34/european-shorthair-8330819_1280.jpg";
+
+var src1 = img1.src;
+var src2 = img2.src;
+
+img1.src = src2;
+img2.src = src1;
+
+});
+
