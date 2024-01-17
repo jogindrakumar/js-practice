3271f5ed4b17b6d9ebbc38a349d83c8fdc39a9cb
jogindrakumar <joginder9868@outlook.com>
2024-01-17 06:41:53 +0530

    previous-code-commented

--- script.js
+++ script.js
@@ -8,21 +8,21 @@
 // } );
 
 
-var img1 = document.querySelector("#img1");
+// var img1 = document.querySelector("#img1");
 
-var img2 = document.querySelector("#img2");
-var btn = document.querySelector("button");
+// var img2 = document.querySelector("#img2");
+// var btn = document.querySelector("button");
 
 
-btn.addEventListener("click", function(){
-// img1.src = "https://cdn.pixabay.com/photo/2024/01/04/21/55/mountain-8488489_1280.jpg";
-// img2.src = "https://cdn.pixabay.com/photo/2023/10/21/06/34/european-shorthair-8330819_1280.jpg";
+// btn.addEventListener("click", function(){
+// // img1.src = "https://cdn.pixabay.com/photo/2024/01/04/21/55/mountain-8488489_1280.jpg";
+// // img2.src = "https://cdn.pixabay.com/photo/2023/10/21/06/34/european-shorthair-8330819_1280.jpg";
 
-var src1 = img1.src;
-var src2 = img2.src;
+// var src1 = img1.src;
+// var src2 = img2.src;
 
-img1.src = src2;
-img2.src = src1;
+// img1.src = src2;
+// img2.src = src1;
 
-});
+// });
 
