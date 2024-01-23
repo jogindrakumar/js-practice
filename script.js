8b34d23e9fcca3ce6dd0912c06a6404764c14714
jogindrakumar <joginder9868@outlook.com>
2024-01-23 07:06:12 +0530

    task-capsule

--- script.js
+++ script.js
@@ -142,3 +142,18 @@
 // p.style.display = "none";
 //    })
 // }
+
+//task = capsule
+var progress = document.querySelector("#progress");
+var h3 = document.querySelector("h3");
+
+var count = 0 ;
+var int = setInterval(function(){
+
+   if(count === 100){
+      clearInterval(int);
+      h3.style.opacity = 1;
+   }
+count++;
+progress.style.width = count + "%";
+},100)
